0ad858a36ac9879be04cee58b869ce89a1ca785c
S.KasqCodes <stephenmacharia1061@gmail.com>
2024-11-25 16:29:43 +0300

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,26 @@
+"""Create a function named calculate_discount(price, discount_percent) that calculates the final price 
+after applying a discount. The function should take the original price (price) and the discount percentage 
+(discount_percent) as parameters. If the discount is 20% or higher, apply the discount; otherwise, 
+return the original price."""
+
+print("Welcome to my week 3 Assignment.")
+
+#Creatinng the function
+
+def calculate_discount(price,discount_percent):
+    if discount_percent >=20:
+        discounted_amount = price - (price * (discount_percent/100))
+        return discounted_amount
+    else:
+        return price
+
+#allowing user to imput price and %Discount for calculation
+try:
+    price = float(input("Input initial price:"))
+    discount_percent = float(input("Input % Discount:"))
+
+    discounted_amount = calculate_discount(price,discount_percent)
+    print(f"The Discounted Amount is: {discounted_amount:.2f}")
+except ValueError:
+    print("Invalid Values! Try Inputing values again.")
+
